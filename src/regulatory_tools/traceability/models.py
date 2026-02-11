9b464da53b42dfbe714bd57234edcb70a73f2474
reneeqian <qian.renee@gmail.com>
2026-02-10 19:16:22 -0800

    pulling out medical_image_ai_toolkit and regulatory_tools

--- src/regulatory_tools/traceability/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import List, Optional
-
-@dataclass
-class TraceRow:
-    requirement_id: str
-    test_id: str
-    evidence_file: Optional[str]
-    result: Optional[str]
